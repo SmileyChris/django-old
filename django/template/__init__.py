a8813df46190e32ab552937c21db3700e6b0af3f
Chris Beaven <smileychris@gmail.com>
2009-12-17 16:35:12 +1300

    Provide access to the root nodelist

--- django/template/__init__.py
+++ django/template/__init__.py
@@ -252,10 +252,13 @@ class Parser(object):
         self.filters = {}
         for lib in builtins:
             self.add_library(lib)
+        self.root_nodelist = None
 
     def parse(self, parse_until=None):
         if parse_until is None: parse_until = []
         nodelist = self.create_nodelist()
+        if self.root_nodelist is None:
+            self.root_nodelist = nodelist
         while self.tokens:
             token = self.next_token()
             if token.token_type == TOKEN_TEXT:
