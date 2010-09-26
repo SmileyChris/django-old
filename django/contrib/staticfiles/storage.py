1610c29cec326193b41358741d61e8cd59c7e35d
Jannis Leidel <jannis@leidel.info>
2010-09-27 00:58:03 +0200

    Raise exception with empty STATICFILES_* settings.

--- django/contrib/staticfiles/storage.py
+++ django/contrib/staticfiles/storage.py
@@ -1,5 +1,6 @@
 import os, posixpath
 
+from django.exceptions import ImproperlyConfigured
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage, get_storage_class
 from django.utils.functional import LazyObject
@@ -12,16 +13,22 @@ class StaticFilesStorage(FileSystemStorage):
     The defaults for ``location`` and ``base_url`` are
     ``STATICFILES_ROOT`` and ``STATICFILES_URL``.
     """
-    staticfiles_location = (settings.STATICFILES_ROOT or
-                            os.path.join(settings.MEDIA_ROOT, 'static'))
-    staticfiles_base_url = (settings.STATICFILES_URL or
-                            posixpath.join(settings.MEDIA_URL, 'static/'))
+    staticfiles_location = settings.STATICFILES_ROOT
+    staticfiles_base_url = settings.STATICFILES_URL
 
     def __init__(self, location=None, base_url=None, *args, **kwargs):
         if location is None:
             location = self.staticfiles_location
         if base_url is None:
             base_url = self.staticfiles_base_url
+        if not location:
+            raise ImproperlyConfigured("You're using the staticfiles app "
+                "without having set the STATICFILES_ROOT setting. Set it to "
+                "the absolute path of the directory that holds static media.")
+        if not base_url:
+            raise ImproperlyConfigured("You're using the staticfiles app "
+                "without having set the STATICFILES_URL setting. Set it to "
+                "URL that handles the files served from STATICFILES_ROOT.")
         super(StaticFilesStorage, self).__init__(location, base_url, *args, **kwargs)
 
 
