f674c865e1caa5a3c6d25d11248db80e01dcb7eb
Chris Beaven <smileychris@gmail.com>
2010-11-01 12:34:28 +1300

    Make new kwarg format consistent across with, include and blocktrans tags

--- django/template/loader_tags.py
+++ django/template/loader_tags.py
@@ -1,6 +1,7 @@
 from django.template import TemplateSyntaxError, TemplateDoesNotExist, Variable
 from django.template import Library, Node, TextNode
-from django.template.defaulttags import kwarg_re
+from django.template.context import Context
+from django.template.defaulttags import token_kwargs
 from django.template.loader import get_template
 from django.conf import settings
 from django.utils.safestring import mark_safe
@@ -128,12 +129,15 @@ class ExtendsNode(Node):
 class BaseIncludeNode(Node):
     def __init__(self, *args, **kwargs):
         self.extra_context = kwargs.pop('extra_context', {})
+        self.isolated_context = kwargs.pop('isolated_context', False)
         super(BaseIncludeNode, self).__init__(*args, **kwargs)
 
     def render_template(self, template, context):
-        values = [(name, var.resolve(context)) for name, var
-                  in self.extra_context.iteritems()]
-        context.update(dict(values))
+        values = dict([(name, var.resolve(context)) for name, var
+                       in self.extra_context.iteritems()])
+        if self.isolated_context:
+            return template.render(Context(values))
+        context.update(values)
         output = template.render(context)
         context.pop()
         return output
@@ -155,9 +159,9 @@ class ConstantIncludeNode(BaseIncludeNode):
         return self.render_template(self.template, context)
 
 class IncludeNode(BaseIncludeNode):
-    def __init__(self, template_name, extra_context={}):
+    def __init__(self, template_name, *args, **kwargs):
+        super(IncludeNode, self).__init__(*args, **kwargs)
         self.template_name = template_name
-        self.extra_context = extra_context
 
     def render(self, context):
         try:
@@ -224,30 +228,41 @@ def do_include(parser, token):
         {% include "foo/some_include" %}
         {% include "foo/some_include" with bar="BAZZ!" baz="BING!" %}
 
+    Use the ``only`` argument to exclude the current context when rendering
+    the included template::
+
+        {% include "foo/some_include" only %}
+        {% include "foo/some_include" with bar="1" only %}
     """
     bits = token.split_contents()
     if len(bits) < 2:
         raise TemplateSyntaxError("%r tag takes at least one argument: the name of the template to be included." % bits[0])
-    namemap = {}
-    if len(bits) > 2:
-        if bits[2] != 'with':
-            raise TemplateSyntaxError('''Invalid option %r for %r tag.''' % (bits[2], bits[0]))
-        for bit in bits[3:]:
-            match = kwarg_re.match(bit)
-            if not match:
-                raise TemplateSyntaxError("Malformed arguments to %r tag." % bits[0])
-            name, value = match.groups()
-            if not name:
-                raise TemplateSyntaxError('''"with" part of %r tag accepts only keyword arguments.''' % bits[0])
-            if name in namemap:
-                raise TemplateSyntaxError("Tried to bind value to name %r more than once." % name)
-            namemap[name] = parser.compile_filter(value)
-        if not namemap:
-            raise TemplateSyntaxError('''"with" in %r tag needs at least one keyword argument.''' % bits[0])
+    options = {}
+    remaining_bits = bits[2:]
+    while remaining_bits:
+        option = remaining_bits.pop(0)
+        if option in options:
+            raise TemplateSyntaxError('The %r option was specified more '
+                                      'than once.' % option)
+        if option == 'with':
+            value = token_kwargs(remaining_bits, parser, support_legacy=False)
+            if not value:
+                raise TemplateSyntaxError('"with" in %r tag needs at least '
+                                          'one keyword argument.' % bits[0])
+        elif option == 'only':
+            value = True
+        else:
+            raise TemplateSyntaxError('Unknown argument for %r tag: %r.' %
+                                      (bits[0], option))
+        options[option] = value
+    isolated_context = options.get('only', False)
+    namemap = options.get('with', {})
     path = bits[1]
     if path[0] in ('"', "'") and path[-1] == path[0]:
-        return ConstantIncludeNode(path[1:-1], extra_context=namemap)
-    return IncludeNode(parser.compile_filter(bits[1]), extra_context=namemap)
+        return ConstantIncludeNode(path[1:-1], extra_context=namemap,
+                                   isolated_context=isolated_context)
+    return IncludeNode(parser.compile_filter(bits[1]), extra_context=namemap,
+                       isolated_context=isolated_context)
 
 register.tag('block', do_block)
 register.tag('extends', do_extends)
