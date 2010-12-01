2bfb3291987c9d9bbb2911eb4c7b85e3af20d508
Chris Beaven <smileychris@gmail.com>
2010-12-01 23:36:48 +1300

    PEP8 line length changes, don't use a unicode string for the safe encode method since it's supposed to be a bytestring anyway, fix the documentation example to be valid.

--- django/http/__init__.py
+++ django/http/__init__.py
@@ -367,24 +367,26 @@ class QueryDict(MultiValueDict):
         """
         Returns an encoded string of all query string arguments.
 
-        :arg safe: Used to specify characters which do not require quoting, e.g.::
+        :arg safe: Used to specify characters which do not require quoting, for
+            example::
 
-            >>> q = QueryDict('', mutable=True)
-            >>> q['next'] = '/a&b/'
-            >>> q.urlencode()
-            'next=%2Fa%26b%2F'
-            >>> q.urlencode(safe='/')
-            'next=/a%26b/'
+                >>> q = QueryDict('', mutable=True)
+                >>> q['next'] = '/a&b/'
+                >>> q.urlencode()
+                'next=%2Fa%26b%2F'
+                >>> q.urlencode(safe='/')
+                'next=/a%26b/'
 
         """
         output = []
         if safe:
-            encode = lambda k, v: u'%s=%s' % ((quote(k, safe), quote(v, safe)))
+            encode = lambda k, v: '%s=%s' % ((quote(k, safe), quote(v, safe)))
         else:
             encode = lambda k, v: urlencode({k: v})
         for k, list_ in self.lists():
             k = smart_str(k, self.encoding)
-            output.extend([encode(k, smart_str(v, self.encoding)) for v in list_])
+            output.extend([encode(k, smart_str(v, self.encoding))
+                           for v in list_])
         return '&'.join(output)
 
 class CompatCookie(SimpleCookie):
